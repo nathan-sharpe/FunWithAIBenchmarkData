cd5058dadd4f8bb0e8200bd1a57bdac1fd7e645e
nathan-sharpe <sharpenn@mail.uc.edu>
2025-03-25 19:03:47 -0300

    Added architecture to the project

---
+++ FunWithAIBenchmarkData/dataVisualizationPackage/imageGenerator.py
@@ -0,0 +1,15 @@
+# Ian's file
+
+# File Name : {required}
+# Student Name: {required}
+# email:  {required}
+# Assignment Number: Assignment nn  {required}
+# Due Date:   {required}
+# Course #/Section:   {required}
+# Semester/Year:   {required}
+# Brief Description of the assignment:  {required}
+
+# Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
+# Citations: {"Stack Overflow" is not sufficient. Provide repeatable links, book page #, etc.}
+
+# Anything else that's relevant:
--- FunWithAIBenchmarkData/mainPackage/main.py
+++ FunWithAIBenchmarkData/mainPackage/main.py
@@ -1,11 +1,24 @@
-# main.py
-# Bill Nicholson
-# nicholdw@ucmail.uc.edu
+# File Name : {required}
+# Student Name: {required}
+# email:  {required}
+# Assignment Number: Assignment nn  {required}
+# Due Date:   {required}
+# Course #/Section:   {required}
+# Semester/Year:   {required}
+# Brief Description of the assignment:  {required}
+
+# Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
+# Citations: {"Stack Overflow" is not sufficient. Provide repeatable links, book page #, etc.}
+
+# Anything else that's relevant:
 
 from readingLevelPackage.readingLevel import Reading_Level
 from utilitiesPackage.utilities import *
 from utilitiesPackage.CSV_Utilities import *
 from PDFPackage.PDFUtilities import *
+from dataVisualizationPackage.dataVisualizationEvan import *
+from dataVisualizationPackage.dataVisualizationNate import *
+from dataVisualizationPackage.imageGenerator import *
 
 if __name__ == "__main__":
 
